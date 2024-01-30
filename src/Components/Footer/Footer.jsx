88dd6ddad1e93f2ba21616873ccc777f85780186
annysen <annysen95@gmail.com>
2024-01-30 16:56:55 +0600

    final

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -29,19 +29,21 @@ const Footer = () => {
       {/* footer down */}
       <FooterDown />
 
-      <div className="flex flex-col md:flex-row justify-between  px-5 md:px-36">
-        <div className="flex">
-          <h1 className="pr-3">made with by a</h1>
-          <img className="w-36 h-7" src={logo} alt="" />
+      <div className="flex flex-col md:flex-row justify-between  px-5 md:pb-5 md:px-36">
+        <div>
+          <a className="flex" href="/">
+            <h1 className="pr-3">made with by a</h1>
+            <img className="w-36 h-7" src={logo} alt="" />
+          </a>
         </div>
         <div className="text-secondary opacity-50">
-          <a href="">
+          <a href="https://www.linkedin.com/">
             <i className="fa-brands fa-linkedin"></i>
           </a>
-          <a className="px-6" href="">
+          <a className="px-6" href="https://www.facebook.com/">
             <i className="fa-brands fa-facebook"></i>
           </a>
-          <a href="">
+          <a href="/">
             <i className="fa-solid fa-globe"></i>
           </a>
         </div>
